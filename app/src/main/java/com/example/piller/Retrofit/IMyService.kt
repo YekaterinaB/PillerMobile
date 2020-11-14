f754b26f35c40e1d5ab55b405f571f24a66bdab5
Yekaterina Batashov <yekaterina.batashov@live.biu.ac.il>
2020-11-14 17:29:43 +0200

    Initial commit

---
+++ app/src/main/java/com/example/piller/Retrofit/IMyService.kt
@@ -0,0 +1,18 @@
+package com.example.piller.Retrofit
+
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
+import retrofit2.http.POST
+
+interface IMyService {
+    @POST ("register")
+    @FormUrlEncoded
+    fun registerUser(@Field("email")email:String,
+                     @Field("name")name:String,
+                     @Field("password")password:String)
+
+    @POST ("login")
+    @FormUrlEncoded
+    fun loginUser(@Field("email")email:String,
+                     @Field("password")password:String)
+}
