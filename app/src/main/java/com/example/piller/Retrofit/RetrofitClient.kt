9d551bf3da5bdd3cc3914451691da1a670b7f0bc
elad g <eladgarber@gmail.com>
2020-11-19 17:16:25 +0200

    added remember me checkbox, and getting the values if the user chose 'remember me' checkbox

--- app/src/main/java/com/example/piller/Retrofit/RetrofitClient.kt
+++ app/src/main/java/com/example/piller/Retrofit/RetrofitClient.kt
@@ -5,11 +5,11 @@ import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.scalars.ScalarsConverterFactory
 
 object RetrofitClient {
-    private var instance: Retrofit?=null
-    fun getInstance():Retrofit{
-        if(instance == null){
-            instance=Retrofit.Builder()
-                .baseUrl("http://10.0.2.2:3000")
+    private var instance: Retrofit? = null
+    fun getInstance(): Retrofit {
+        if (instance == null) {
+            instance = Retrofit.Builder()
+                .baseUrl("http://10.0.2.2:4000")
                 .addConverterFactory(ScalarsConverterFactory.create())
                 .addCallAdapterFactory(RxJava2CallAdapterFactory.create()).build()
         }
