2b0f9b0c6a39a927434fee98d8f4f281fd473030
elad g <eladgarber@gmail.com>
2020-11-20 17:35:39 +0200

    created weekly CalendarActivity.kt

--- app/src/main/java/com/example/piller/activities/CalendarActivity.kt
+++ app/src/main/java/com/example/piller/activities/CalendarActivity.kt
@@ -2,6 +2,8 @@ package com.example.piller.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+import androidx.core.content.ContextCompat
 import com.example.piller.R
 import com.example.piller.utilities.*
 
@@ -9,12 +11,18 @@ class CalendarActivity : AppCompatActivity() {
     private lateinit var loggedUserEmail: String
     private lateinit var loggedUserName: String
 
+    lateinit var toolbar: Toolbar
+
     override fun onCreate(savedInstanceState: Bundle?) {
         //  todo: disable going back to login
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_calendar)
 
+        toolbar = findViewById(R.id.calendar_toolbar)
+
         loggedUserEmail = intent.getStringExtra(DbConstants.LOGGED_USER_EMAIL)!!
         loggedUserName = intent.getStringExtra(DbConstants.LOGGED_USER_NAME)!!
+        toolbar.title = loggedUserName
+        setSupportActionBar(toolbar)
     }
 }
