865ba6719ed9c56834b181436af4795ae37be2f3
Yekaterina Batashov <yekaterina.batashov@live.biu.ac.il>
2020-11-20 16:03:13 +0200

    calendar api

--- app/src/main/java/com/example/piller/activities/CalendarActivity.kt
+++ app/src/main/java/com/example/piller/activities/CalendarActivity.kt
@@ -1,4 +1,15 @@
 package com.example.piller.activities
 
-class CalendarActivity {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.piller.R
+
+class CalendarActivity : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+
 }
