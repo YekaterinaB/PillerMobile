3fb2e707f48f3f41eca0ca695daff42e870c0bc7
Yekaterina Batashov <yekaterina.batashov@live.biu.ac.il>
2020-11-25 22:26:07 +0200

    get week events

--- app/src/main/java/com/example/piller/EventInterpreter.kt
+++ app/src/main/java/com/example/piller/EventInterpreter.kt
@@ -1,7 +1,10 @@
 package com.example.piller
 
 import com.example.piller.models.CalendarEvent
+import com.google.gson.JsonArray
+import com.squareup.moshi.Json
 import org.json.JSONArray
+import org.json.JSONObject
 import java.util.*
 import java.util.concurrent.TimeUnit
 
@@ -11,40 +14,91 @@ class EventInterpreter {
         start: Date,
         end: Date,
         drugList: JSONArray
-    ): Array<List<CalendarEvent>> {
-        val millionSeconds = end.time - start.time
-        val daysBetween = TimeUnit.MILLISECONDS.toDays(millionSeconds) as Int
+    ): Array<MutableList<CalendarEvent>> {
+        val daysBetween = getDaysBetween(start,end)+1
 
-        var eventList = Array(daysBetween, { emptyList<CalendarEvent>() })
+        var eventList = Array(daysBetween, { mutableListOf<CalendarEvent>() })
         for (i in 0 until drugList.length()) {
             val drug = drugList.getJSONObject(i)
             val drugName = drug.get("drug") as String
-            val drugInfo = drug.get("drug_info") as Map<String, String>
-            val drugEvent = getDrugEvent(drugName, drugInfo, start, end)
-            if (drugEvent != null) {
-                val day = drugEvent.index_day
-                eventList[day].toMutableList().add(drugEvent)
+            val drugInfo = drug.get("drug_info") as JSONObject
+            val drugEventList = getDrugEvent(drugName, drugInfo, start, end)
+            // put all event in array
+            if (! drugEventList.isEmpty()) {
+                for(j in 0 until drugEventList.size){
+                    val indexDay=drugEventList[j].index_day
+                    eventList[indexDay].add(drugEventList[j])
+                }
             }
         }
         return eventList
     }
 
+    private fun getDaysBetween(first:Date,second:Date) :Int{
+        val calendarFirst=Calendar.getInstance()
+        calendarFirst.time=first
+
+        val calendarSecond=Calendar.getInstance()
+        calendarSecond.time=second
+        return calendarSecond.get(Calendar.DAY_OF_WEEK)- calendarFirst.get(Calendar.DAY_OF_WEEK)
+    }
+
     private fun getDrugEvent(
         drugName: String,
-        drugInfo: Map<String, String>,
+        drugInfo: JSONObject,
         start: Date,
         end: Date
-    ): CalendarEvent? {
-        val repeatStart = drugInfo.get("repeat_start")
-        val repeatYear = (drugInfo.get("repeat_year") as String).split(',')
-        val repeatMonth = (drugInfo.get("repeat_month") as String).split(',')
-        val repeatDay = (drugInfo.get("repeat_day") as String).split(',')
-        val repeatWeek = (drugInfo.get("repeat_week") as String).split(',')
-        val repeatWeekday = (drugInfo.get("repeat_weekday") as String).split(',')
+    ): MutableList<CalendarEvent>{
+        val eventList:MutableList<CalendarEvent> = mutableListOf()
+        val repeatStart = (drugInfo.get("repeat_start") as String).toLong()
+        val repeatYear = (drugInfo.get("repeat_year") as String).split(',').map(String::toInt)
+        val repeatMonth = (drugInfo.get("repeat_month") as String).split(',').map(String::toInt)
+        val repeatDay = (drugInfo.get("repeat_day") as String).split(',').map(String::toInt)
+        val repeatWeek = (drugInfo.get("repeat_week") as String).split(',').map(String::toInt)
+        val repeatWeekday = (drugInfo.get("repeat_weekday") as String).split(',').map(String::toInt)
+        var calendarCurrent=Calendar.getInstance()
+        calendarCurrent.time=start
+        val calendarStartRepeat=Calendar.getInstance()
 
+        // if the start intake is after start day
+        calendarStartRepeat.setTimeInMillis(repeatStart)
+        if(calendarCurrent.time.before(calendarStartRepeat.time)){
+            calendarCurrent=calendarStartRepeat
+        }
 
+        // check days between actual start date and the new start date
+        var indexDay=getDaysBetween(start,calendarCurrent.time)
 
-        return null
+        while( calendarCurrent.time < end){
+            val dayOfWeek: Int = calendarCurrent.get(Calendar.DAY_OF_WEEK)
+            val numberOfWeek: Int= calendarCurrent.get(Calendar.WEEK_OF_MONTH)
+            val dayOfMonth: Int = calendarCurrent.get(Calendar.DAY_OF_MONTH)
+            val monthOfYear: Int =calendarCurrent.get(Calendar.MONTH)
+            val year:Int =calendarCurrent.get(Calendar.YEAR)
+            if(isInRepeat(year,repeatYear) && isInRepeat(monthOfYear,repeatMonth)&&
+                isInRepeat(dayOfMonth,repeatDay)&&isInRepeat(numberOfWeek,repeatWeek)&&
+                isInRepeat(dayOfWeek,repeatWeekday)){
+                    //event is in repeats
+                    val event= CalendarEvent(drugName,indexDay,calendarCurrent.time,false)
+                    //todo is taken
+                    eventList.add(event)
+                }
+
+            calendarCurrent.add(Calendar.DATE,1)
+            indexDay+=1
+        }
+        return eventList
+    }
+
+    private fun isInRepeat(current:Int,repeat:List<Int>):Boolean{
+        var isIn=false
+        for (i in 0 until repeat.size) {
+            if(repeat[i] == -1 || repeat[i] == current){
+                isIn=true
+                break
+            }
+        }
+        return isIn
     }
 
     public fun getFirstDayOfWeek(): Date {
