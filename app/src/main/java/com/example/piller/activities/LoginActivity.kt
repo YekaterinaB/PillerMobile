68a00b80e5bf56dfa32d29286351969c3d78b336
Yekaterina <katya.btsh@gmail.com>
2021-04-16 14:14:08 +0300

    login stay loggedin

--- app/src/main/java/com/example/piller/activities/LoginActivity.kt
+++ app/src/main/java/com/example/piller/activities/LoginActivity.kt
@@ -1,6 +1,7 @@
 package com.example.piller.activities
 
 import android.os.Bundle
+import android.view.View
 import android.widget.RelativeLayout
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.NotificationManagerCompat
@@ -10,12 +11,13 @@ import com.example.piller.R
 import com.example.piller.accountManagement.AppPreferences
 import com.example.piller.api.ServiceBuilder
 import com.example.piller.fragments.LoginFragments.SplashScreenFragment
+import com.example.piller.notif.AlarmScheduler
 import com.example.piller.notif.NotificationHelper
 import com.example.piller.utilities.DbConstants
 import com.example.piller.viewModels.MainActivityViewModel
 import io.reactivex.disposables.CompositeDisposable
 
-class LoginActivity: AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     private var _compositeDisposable = CompositeDisposable()
     lateinit var _loadingScreen: RelativeLayout
     private lateinit var _viewModel: MainActivityViewModel
@@ -35,10 +37,21 @@ class LoginActivity: AppCompatActivity() {
         AppPreferences.init(this)
         ServiceBuilder.updateRetrofit(DbConstants.SERVER_URL)
         createChannelForNotification()
+        stayLoggedIn()
         initializeFragment(savedInstanceState)
     }
 
-
+    private fun stayLoggedIn() {
+        if (AppPreferences.stayLoggedIn) {
+            //  run the background service (it has to run from the application for one time so it'll
+            //  be able to tun when the device reboots
+            AlarmScheduler.runBackgroundService(this, AppPreferences.email)
+            _loadingScreen.visibility = View.VISIBLE
+            //  stayloggedin
+            AppPreferences.stayLoggedIn = true
+            _viewModel.loginUser(AppPreferences.email, AppPreferences.password)
+        }
+    }
 
     private fun createChannelForNotification() {
         NotificationHelper.createNotificationChannel(
