6d033dd6c5f4a75e4079e5f565bfa286434aa242
eladg7 <eladgarber@gmail.com>
2021-04-21 20:08:38 +0300

    updated popup ui and removed FAB

--- app/src/main/java/com/example/piller/fragments/WeeklyCalendarFragment.kt
+++ app/src/main/java/com/example/piller/fragments/WeeklyCalendarFragment.kt
@@ -2,13 +2,10 @@ package com.example.piller.fragments
 
 import android.app.Activity
 import android.content.Intent
-import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.view.animation.Animation
-import android.view.animation.AnimationUtils
 import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.activityViewModels
@@ -28,7 +25,6 @@ import com.example.piller.utilities.DateUtils
 import com.example.piller.utilities.DbConstants
 import com.example.piller.viewModels.ProfileViewModel
 import com.example.piller.viewModels.WeeklyCalendarViewModel
-import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 
 class WeeklyCalendarFragment : FragmentWithUserObject() {
@@ -37,33 +33,8 @@ class WeeklyCalendarFragment : FragmentWithUserObject() {
 
     private val DRUG_INFO_INTENT_ID = 1
 
-    // add drug animation
-    private val _rotateOpen: Animation by lazy {
-        AnimationUtils.loadAnimation(this.context, R.anim.rotate_open_anim)
-    }
-    private val _rotateClose: Animation by lazy {
-        AnimationUtils.loadAnimation(this.context, R.anim.rotate_close_anim)
-    }
-    private val _fromBottom: Animation by lazy {
-        AnimationUtils.loadAnimation(this.context, R.anim.from_bottom_anim)
-    }
-    private val _toBottom: Animation by lazy {
-        AnimationUtils.loadAnimation(this.context, R.anim.to_botton_anim)
-    }
-
-    private var FABClicked = false
-
     private lateinit var _fragmentView: View
     private lateinit var currentCalendarEvent: CalendarEvent
-
-    private lateinit var newDrugFAB: FloatingActionButton
-    private lateinit var newDrugCameraFAB: FloatingActionButton
-    private lateinit var newDrugBoxFAB: FloatingActionButton
-    private lateinit var newDrugNameFAB: FloatingActionButton
-    private lateinit var newDrugLayout: ConstraintLayout
-    private lateinit var newDrugCameraLabel: TextView
-    private lateinit var newDrugBoxLabel: TextView
-    private lateinit var newDrugNameLabel: TextView
 
     private lateinit var daysContainer: RecyclerView
     private lateinit var daysAdapter: WeeklyDayAdapter
@@ -75,7 +46,6 @@ class WeeklyCalendarFragment : FragmentWithUserObject() {
     ): View? {
         _fragmentView = inflater.inflate(R.layout.fragment_weekly_calendar, container, false)
         initViews(_fragmentView)
-        initListeners()
         initObservers()
         _weeklyCalendarViewModel.getWeekEvents(
             _loggedUserObject,
@@ -144,123 +114,7 @@ class WeeklyCalendarFragment : FragmentWithUserObject() {
             }
     }
 
-    private fun initListeners() {
-        newDrugFAB.setOnClickListener {
-            onNewDrugFABClicked()
-        }
-
-        newDrugCameraFAB.setOnClickListener {
-            showAddNewDrugActivity(DbConstants.DRUG_BY_PILL)
-        }
-
-        newDrugBoxFAB.setOnClickListener {
-            showAddNewDrugActivity(DbConstants.DRUG_BY_BOX)
-        }
-
-        newDrugNameFAB.setOnClickListener {
-            showAddNewDrugActivity(DbConstants.DRUG_BY_NAME)
-        }
-
-        newDrugLayout.setOnClickListener {
-            FABLayoutClicked()
-        }
-    }
-
-    /**
-     * @fragmentID: says which fragment AddNewDrugActivity should show
-     */
-    private fun showAddNewDrugActivity(addType: String) {
-        val intent = Intent(activity, AddNewDrugActivity::class.java)
-        intent.putExtra(DbConstants.ADD_DRUG_TYPE, addType)
-        putLoggedUserObjectInIntent(intent)
-        intent.putExtra(DbConstants.CALENDAR_ID, _weeklyCalendarViewModel.calendarId)
-        startActivity(intent)
-    }
-
-    private fun FABLayoutClicked() {
-        if (FABClicked) {
-            setFABVisibility(FABClicked)
-            setFABAnimation(FABClicked)
-            setFABClickable(FABClicked)
-            newDrugLayout.setBackgroundColor(Color.TRANSPARENT)
-            newDrugLayout.visibility = View.GONE
-            FABClicked = !FABClicked
-        }
-    }
-
-    private fun onNewDrugFABClicked() {
-        setFABVisibility(FABClicked)
-        setFABAnimation(FABClicked)
-        setFABClickable(FABClicked)
-        FABClicked = !FABClicked
-    }
-
-    private fun setFABVisibility(clicked: Boolean) {
-        if (!clicked) {
-            newDrugCameraFAB.visibility = View.INVISIBLE
-            newDrugCameraLabel.visibility = View.INVISIBLE
-            newDrugBoxFAB.visibility = View.INVISIBLE
-            newDrugNameFAB.visibility = View.INVISIBLE
-            newDrugNameLabel.visibility = View.INVISIBLE
-            newDrugBoxLabel.visibility = View.INVISIBLE
-            newDrugLayout.visibility = View.VISIBLE
-            newDrugLayout.setBackgroundColor(Color.parseColor("#B3000000"))
-        } else {
-            newDrugCameraFAB.visibility = View.VISIBLE
-            newDrugCameraLabel.visibility = View.VISIBLE
-            newDrugBoxFAB.visibility = View.VISIBLE
-            newDrugNameFAB.visibility = View.VISIBLE
-            newDrugNameLabel.visibility = View.VISIBLE
-            newDrugBoxLabel.visibility = View.VISIBLE
-            newDrugLayout.visibility = View.GONE
-            newDrugLayout.setBackgroundColor(Color.TRANSPARENT)
-        }
-    }
-
-    private fun setFABAnimation(clicked: Boolean) {
-        if (!clicked) {
-            newDrugBoxFAB.startAnimation(_fromBottom)
-            newDrugCameraFAB.startAnimation(_fromBottom)
-            newDrugCameraLabel.startAnimation(_fromBottom)
-            newDrugNameFAB.startAnimation(_fromBottom)
-            newDrugBoxLabel.startAnimation(_fromBottom)
-            newDrugNameLabel.startAnimation(_fromBottom)
-            newDrugFAB.startAnimation(_rotateOpen)
-        } else {
-            newDrugBoxFAB.startAnimation(_toBottom)
-            newDrugCameraFAB.startAnimation(_toBottom)
-            newDrugCameraLabel.startAnimation(_toBottom)
-            newDrugNameFAB.startAnimation(_toBottom)
-            newDrugNameLabel.startAnimation(_toBottom)
-            newDrugBoxLabel.startAnimation(_toBottom)
-            newDrugFAB.startAnimation(_rotateClose)
-        }
-    }
-
-    private fun setFABClickable(clicked: Boolean) {
-        if (!clicked) {
-            newDrugCameraFAB.isClickable = true
-            newDrugBoxFAB.isClickable = true
-            newDrugNameFAB.isClickable = true
-        } else {
-            newDrugCameraFAB.isClickable = false
-            newDrugBoxFAB.isClickable = false
-            newDrugNameFAB.isClickable = false
-        }
-    }
-
     private fun initViews(fragment: View) {
-        newDrugFAB = fragment.findViewById(R.id.calendar_new_drag_fab)
-        newDrugCameraFAB = fragment.findViewById(R.id.calendar_add_drug_camera)
-        newDrugBoxFAB = fragment.findViewById(R.id.calendar_add_drug_box)
-        newDrugNameFAB = fragment.findViewById(R.id.calendar_add_drug_name)
-
-        newDrugLayout = fragment.findViewById(R.id.calendar_fab_root)
-
-        newDrugCameraLabel = fragment.findViewById(R.id.calendar_drug_camera_label)
-        newDrugBoxLabel = fragment.findViewById(R.id.calendar_drug_box_label)
-        newDrugNameLabel = fragment.findViewById(R.id.calendar_drug_name_label)
-
         daysContainer = fragment.findViewById(R.id.weekly_days_container)
         initWeeklyDaysContainer()
     }
